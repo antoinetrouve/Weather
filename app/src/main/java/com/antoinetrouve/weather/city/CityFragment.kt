7ed0ab6968fda14eb3695ee4933df11b98e4ddb4
Antoine Trouvé <antrouve.pro@gmail.com>
2018-11-20 12:56:13 +0100

    create city base element

---
+++ app/src/main/java/com/antoinetrouve/weather/city/CityFragment.kt
@@ -0,0 +1,17 @@
+package com.antoinetrouve.weather.city
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.antoinetrouve.weather.R
+
+class CityFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_city, container, false)
+
+        return view
+    }
+}
