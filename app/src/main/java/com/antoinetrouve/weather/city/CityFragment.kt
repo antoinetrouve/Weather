e3ec0421f27f66ab42a85a62184f6898568d74b7
Antoine Trouvé <antrouve.pro@gmail.com>
2018-11-20 20:09:06 +0100

    create Recyclerview and set Adapter

--- app/src/main/java/com/antoinetrouve/weather/city/CityFragment.kt
+++ app/src/main/java/com/antoinetrouve/weather/city/CityFragment.kt
@@ -2,16 +2,20 @@ package com.antoinetrouve.weather.city
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.*
 import android.widget.Toast
 import com.antoinetrouve.weather.App
 import com.antoinetrouve.weather.Database
 import com.antoinetrouve.weather.R
 
-class CityFragment : Fragment() {
+class CityFragment : Fragment(), CityAdapter.CityItemListener {
 
     private lateinit var  cities: MutableList<City>
     private lateinit var database: Database
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: CityAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,8 +27,17 @@ class CityFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_city, container, false)
+        val recyclerView = view.findViewById<RecyclerView>(R.id.cities_recycler_view)
+        recyclerView.layoutManager = LinearLayoutManager(context)
 
         return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        adapter = CityAdapter(cities, this)
+        recyclerView.adapter = adapter
     }
 
     override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
@@ -42,6 +55,15 @@ class CityFragment : Fragment() {
 
         return super.onOptionsItemSelected(item)
     }
+
+    override fun onCitySelected(city: City) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onCityDeleted(city: City) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
 
     private fun showCreateCityDialog() {
         val createCityFragment = CreateCityDialogFragment()
